516781e33179bfd7dce00e402ec55a01c4b54689
Alexander <lassonalexander@gmail.com>
2025-06-23 01:21:51 -0400

    caret implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,50 @@
 import { fakerDE as faker } from '@faker-js/faker';
 import RestartButton from './components/RestartButton';
 import Results from './components/Results';
-  
-const words = faker.lorem.words(10);
+import UserTypings from './components/UserTypings';
 
+const words = faker.lorem.words(10);
+// main app component 
 const App = () => {
   return (
     <>
-    <CountDownTimer timeLeft={30} />
-    <GeneratedWords words={words} />
-    <RestartButton
-      className={"mx-auto mt-10 text-slate-900"}
-      onRestart={() => null}
+      <CountDownTimer timeLeft={30} />
+      <WordsContainer>
+        <GeneratedWords words={words} />
+        <UserTypings className="absolute inset-0" userInput={"test"} />
+      </WordsContainer>
+      <RestartButton
+        className={"mx-auto mt-10 text-slate-500"}
+        onRestart={() => null}
       />
       <Results
         className="mt-10"
         errors={10}
         accuracyPercentage={100}
         total={200}
-        />
+      />
     </>
   );
 };
 
 
-const GeneratedWords = ({ words }: {words: string}) => {
+const WordsContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="text-4xl text-center text-slate-500 ">{words}</div>
+    <div className="relative text-3xl max-w-xl leading-relaxed break-all mt-3">
+      {children}
+    </div>
   )
 }
 
-const CountDownTimer = ({timeLeft}: {timeLeft: number }) => {
+
+
+const GeneratedWords = ({ words }: { words: string }) => {
+  return (
+    <div className="text-slate-500 ">{words}</div>
+  )
+}
+
+const CountDownTimer = ({ timeLeft }: { timeLeft: number }) => {
   return <h2 className="text-primary-500 font-medium text-center text-xl text-slate-600"> Time: {timeLeft}</h2>;
 }
 
