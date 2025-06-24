0efd5b049e547fc7a4f3383ff9f1903ac3c89626
Alexander <lassonalexander@gmail.com>
2025-06-23 22:11:40 -0400

    helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,24 @@
 export const formatPercentage = (percentage: number) => {
-   return percentage.toFixed(0) + "%";
+  return percentage.toFixed(0) + "%";
 };
+
+export const countErrors = (actual: string, expected: string) => {
+  const expectedCharacters = expected.split("");
+
+  return expectedCharacters.reduce((errors, expectedChar, i) => {
+    const actualChar = actual[i];
+    if (actualChar !== expectedChar) {
+      errors++;
+    }
+    return errors;
+  }, 0);
+};
+
+export const calculatAccuracyPercentage = (errors: number, total: number) => {
+  if (total > 0) {
+    const corrects = total - errors;
+    return ((total - errors) / total) * 100;
+  }
+  return 0;
+};
+
