476369beabe2add678f5b5e7957d2167e5ea7149
Alexander <lassonalexander@gmail.com>
2025-06-23 21:36:29 -0400

    keydown handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RestartButton from './components/RestartButton';
 import Results from './components/Results';
 import UserTypings from './components/UserTypings';
 import useEngine from './hooks/useEngine';
-
+// vid time 31:43. we are fixing useTypings.
 
 const words = faker.lorem.words(10);
 // main app component 
